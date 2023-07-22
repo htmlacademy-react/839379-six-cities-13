09540aa81684773b6d15075c6c792dd31dc4f974
Artem Minaliev <artemminaliev@gmail.com>
2023-07-22 13:26:36 +0300

    правки в типизации

--- src/types/offer.ts
+++ src/types/offer.ts
@@ -1,30 +1,12 @@
-export type Offer = {
-	id: string;
-	title: string;
-	type: string;
-	price: number;
-	city: City;
-	location: PointLocation;
-	isFavorite: boolean;
-	isPremium: boolean;
-	rating: number;
+import { Place } from './place';
+
+export type Offer = Omit<Place, 'previewImage'> & {
 	description: string;
 	bedrooms: number;
 	goods: string[];
 	host: Host;
 	images: string[];
 	maxAdults: number;
-}
-
-export type City = {
-	name: string;
-	location: PointLocation;
-}
-
-export type PointLocation = {
-	latitude: number;
-	longitude: number;
-	zoom: number;
 }
 
 export type Host = {
