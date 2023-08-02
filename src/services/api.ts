4d540e5334a3ce03959ca684d76887f8eebeff20
Artem Minaliev <artemminaliev@gmail.com>
2023-08-02 20:54:24 +0300

    добавлена поддержка токенов

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,20 @@
 import axios, {AxiosInstance} from 'axios';
 import { BASE_URL, TIMEOUT} from '../const';
+import { getToken } from './token';
 
 export const createAPI = (): AxiosInstance => {
 	const api = axios.create({
 		baseURL: BASE_URL,
 		timeout: TIMEOUT
+	});
+
+	api.interceptors.request.use((config) => {
+		const token = getToken();
+		if(token && config.headers) {
+			config.headers['X-Token'] = token;
+		}
+
+		return config;
 	});
 
 	return api;
