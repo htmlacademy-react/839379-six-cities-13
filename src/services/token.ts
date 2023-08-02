6b47121c99910de8a806af52d93cb93fbae637e4
Artem Minaliev <artemminaliev@gmail.com>
2023-08-02 20:11:46 +0300

    создан модуль для работы с токеном

---
+++ src/services/token.ts
@@ -0,0 +1,20 @@
+const AUTH_TOKEN_KEY = 'six-cities';
+
+export type Token = string;
+
+const getToken = (): Token => {
+	const token = localStorage.getItem(AUTH_TOKEN_KEY);
+	return token ?? '';
+};
+
+const saveToken = (token: Token): void => {
+	localStorage.setItem(AUTH_TOKEN_KEY, token);
+};
+
+const dropToken = (): void => {
+	localStorage.removeItem(AUTH_TOKEN_KEY);
+};
+
+export {getToken, saveToken, dropToken};
+
+
