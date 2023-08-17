5d41943d022b96de20acc5c04e264712def5b5ed
Artem Minaliev <artemminaliev@gmail.com>
2023-08-17 11:29:06 +0300

    добавлен компонент footer

--- src/pages/favorites-page/favorites-page.tsx
+++ src/pages/favorites-page/favorites-page.tsx
@@ -5,6 +5,7 @@ import { getFavorites } from '../../store/favorites-data/selectors';
 import EmptyFavoritesList from '../../components/empty-favorites-list/empty-favorites-list';
 import cn from 'classnames';
 import PlaceCard from '../../components/place-card/place-card';
+import Footer from '../../components/footer/footer';
 
 function FavoritePage(): JSX.Element {
 	const favoritePlaces = useAppSelector(getFavorites);
@@ -51,17 +52,7 @@ function FavoritePage(): JSX.Element {
 					</div>
 				</main>
 			)}
-			<footer className="footer container">
-				<a className="footer__logo-link" href="main.html">
-					<img
-						className="footer__logo"
-						src="img/logo.svg"
-						alt="6 cities logo"
-						width={64}
-						height={33}
-					/>
-				</a>
-			</footer>
+			<Footer favorites={favoritePlaces}/>
 		</div>
 	);
 }
